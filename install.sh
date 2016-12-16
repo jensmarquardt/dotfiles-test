e2dfa54052d1ef88f9afe01e32633df0597b2dcb
Jens Marquardt <dev@jensmarquardt.com>
2016-12-16 23:50:40 +0100

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 run_install() {
 	curl --progress-bar --location 'https://github.com/jensmarquardt/dotfiles-test/archive/master.zip' | ditto -xk - '/tmp'
-    
+
     # source all shell scripts
     for shell_script in '/tmp/dotfiles-test-master/scripts/'*.sh; do
         source "${shell_script}"
