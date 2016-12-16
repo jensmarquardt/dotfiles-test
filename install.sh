577a3da288e193318a0f328e2060c61fa3006527
Jens Marquardt <dev@jensmarquardt.com>
2016-12-16 23:07:24 +0100

    Remove spaces

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 run() {
 	# source all shell scripts
-  	for shell_script in 'scripts/'*.sh; do
-    	source "${shell_script}"
-  	done
-
-  	setup
+	for shell_script in 'scripts/'*.sh; do
+		source "${shell_script}"
+	done
+	
+	setup
 }
 
 run
